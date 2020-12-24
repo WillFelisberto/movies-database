4bd9a7ff4b0af38a6dade94bb37c3627f3b1364c
Willian Souza <willian_aru_@hotmail.com>
2020-12-24 12:23:38 -0300

    fix: remove not used variables

--- src/App.js
+++ src/App.js
@@ -1,72 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import { getPopularMovie, getGenres } from './api/services';
-import Pagination from '@material-ui/lab/Pagination';
-import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme } from '@material-ui/core';
-import Card from './components/Card';
-import Filter from './components/Filter';
-import { Cards, ContainerBody } from './theme/styled';
-import allActions from './store/actions';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { getPopularMovie, getGenres } from "./api/services";
+import Pagination from "@material-ui/lab/Pagination";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core";
+import Card from "./components/Card";
+// import Filter from './components/Filter';
+import { Cards, ContainerBody } from "./theme/styled";
+import allActions from "./store/actions";
+import { useSelector, useDispatch } from "react-redux";
 
 //TODO: ajustar checkbox dos filtros(modal ou sidebar)
 //TODO: infinite scroll
 export default function App() {
-	const [movies, setMovies] = useState([]);
-	const [genres, setGenres] = useState([]);
-	const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-	const filterState = useSelector((state) => state.filter.dados);
-	const pageState = useSelector((state) => state.pages.dados);
+  const filterState = useSelector((state) => state.filter.dados);
+  const pageState = useSelector((state) => state.pages.dados);
 
-	const [filter, setFilter] = useState(filterState);
+  const [filter, setFilter] = useState(filterState);
 
-	useEffect(() => {
-		const getMovies = async () => {
-			const res = await getPopularMovie();
-			const resGenges = await getGenres();
-			setMovies(res);
-			setGenres(resGenges.genres);
-			setLoading(true);
-		};
-		getMovies();
-	}, [filterState]);
+  useEffect(() => {
+    const getMovies = async () => {
+      const res = await getPopularMovie();
+      const resGenges = await getGenres();
+      setMovies(res);
+      setGenres(resGenges.genres);
+      setLoading(true);
+    };
+    getMovies();
+  }, [filterState]);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const handlePageClick = async (page, value) => {
-		const res = await getPopularMovie(value);
-		setMovies(res);
-		setLoading(true);
+  const handlePageClick = async (page, value) => {
+    const res = await getPopularMovie(value);
+    setMovies(res);
+    setLoading(true);
 
-		dispatch(allActions.pagesActions.definePage(value));
-	};
+    dispatch(allActions.pagesActions.definePage(value));
+  };
 
-	const handleFilter = (e) => {
-		const checkValue = Number(e.target.value);
-		const teste = e.target.checked;
+  // const handleFilter = (e) => {
+  // 	const checkValue = Number(e.target.value);
+  // 	const teste = e.target.checked;
 
-		if (!teste) {
-			dispatch(allActions.filterActions.removeFilter(checkValue));
-			setFilter(filterState);
-		} else {
-			setFilter([...filter, checkValue]);
-			dispatch(
-				allActions.filterActions.defineFilter([...filter, checkValue])
-			);
-		}
-	};
+  // 	if (!teste) {
+  // 		dispatch(allActions.filterActions.removeFilter(checkValue));
+  // 		setFilter(filterState);
+  // 	} else {
+  // 		setFilter([...filter, checkValue]);
+  // 		dispatch(
+  // 			allActions.filterActions.defineFilter([...filter, checkValue])
+  // 		);
+  // 	}
+  // };
 
-	const theme = createMuiTheme({
-		palette: {
-			type: 'dark',
-		},
-	});
-	return (
-		<ThemeProvider theme={theme}>
-			{loading ? (
-				<>
-					{/* {genres.map((el) => (
+  const theme = createMuiTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      {loading ? (
+        <>
+          {/* {genres.map((el) => (
 						<Filter
 							setFilterValue={(e) => handleFilter(e)}
 							key={el.id}
@@ -75,26 +75,26 @@ export default function App() {
 						/>
 					))} */}
 
-					<ContainerBody>
-						<Cards>
-							{movies.results.map((el) => (
-								<Card
-									key={el.id}
-									movie={el}
-									filter={filter}
-									Allgenres={genres}
-								/>
-							))}
-						</Cards>
-						<Pagination
-							color='primary'
-							count={movies.total_pages}
-							page={pageState}
-							onChange={handlePageClick}
-						/>
-					</ContainerBody>
-				</>
-			) : null}
-		</ThemeProvider>
-	);
+          <ContainerBody>
+            <Cards>
+              {movies.results.map((el) => (
+                <Card
+                  key={el.id}
+                  movie={el}
+                  filter={filter}
+                  Allgenres={genres}
+                />
+              ))}
+            </Cards>
+            <Pagination
+              color="primary"
+              count={movies.total_pages}
+              page={pageState}
+              onChange={handlePageClick}
+            />
+          </ContainerBody>
+        </>
+      ) : null}
+    </ThemeProvider>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, browserHistory } from 'react-router-dom';
-import App from './App';
-import DetailsMovie from './components/DetailsMovie/DetailsMovie';
-import PageNotFound from './components/PageNotFound';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import GlobalStyle from './theme/globalStyles';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./App";
+import DetailsMovie from "./components/DetailsMovie/DetailsMovie";
+import PageNotFound from "./components/PageNotFound";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import GlobalStyle from "./theme/globalStyles";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
-				<GlobalStyle />
-				<Header />
-				<Switch>
-					<Route
-						path='/movie/:id'
-						exact={true}
-						component={DetailsMovie}
-					/>
-					<Route path='/' exact={true} component={App} />
-					<Route component={PageNotFound} />
-				</Switch>
-				<Footer />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Header />
+        <Switch>
+          <Route path="/movie/:id" exact={true} component={DetailsMovie} />
+          <Route path="/" exact={true} component={App} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
