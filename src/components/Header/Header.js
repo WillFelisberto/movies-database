010b11259663d6c83f00248e0063483314154ed2
Willian Souza <willian_aru_@hotmail.com>
2020-12-24 12:39:23 -0300

    fix: insert link from react router dom

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { ContainerHeader } from './styled';
+import React from "react";
+import { Link } from "react-router-dom";
+import { ContainerHeader } from "./styled";
 //TODO: Deixar cabeçalho ajeitado
 export default function Header() {
-	return (
-		<ContainerHeader>
-			<a href='/'>
-				<h1>Movies DataBase</h1>
-			</a>
-		</ContainerHeader>
-	);
+  return (
+    <ContainerHeader>
+      <Link to={"/"}>
+        <h1>Movies DataBase</h1>
+      </Link>
+    </ContainerHeader>
+  );
 }
